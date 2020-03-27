c0dd15b97fe459216fe63f06e68bf7a839a04105
Henrique Arsego <henrique.arsego@gmail.com>
2020-03-26 21:33:52 -0300

    Create tipos-dados.php

---
+++ tipos-dados.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>php -S localhost:4040</title>
+  </head>
+  <body>
+    <?php
+      $frase = "To be or not to be";
+      $inteiro = 30;
+      $float = 3.7;
+      $boolean = true;      
+     ?>
+  </body>
+</html>
